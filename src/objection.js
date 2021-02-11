0d8754e7b0a10a559cb58e3b7f0dd3188dc0a54a
Brian <briancbarrow@gmail.com>
2021-02-11 10:29:11 -0700

    made connection an object

--- src/objection.js
+++ src/objection.js
@@ -4,7 +4,14 @@ const knex = require('knex');
 module.exports = function (app) {
   let { client, connection } = app.get('postgres');
   if(process.env.NODE_ENV === 'production') {
-    connection += '?ssl=true&rejectUnauthorized=false'
+    connection = {
+      host : 'process.env.DB_HOST',
+      user : 'process.env.DB_USER',
+      password : 'process.env.DB_PWD',
+      database : 'process.env.DB_NAME',
+      ssl: true,
+      rejectUnauthorized: false
+    }
   }
   console.log('CLIENT', client)
   console.log('CONNNECTION', connection)
