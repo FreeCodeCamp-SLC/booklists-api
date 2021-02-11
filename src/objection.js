54053bf9fb4b93c0073957dd356d50e23e3d3380
Brian <briancbarrow@gmail.com>
2021-02-11 10:31:45 -0700

    made env variables NOT strings. :facepalm:

--- src/objection.js
+++ src/objection.js
@@ -5,10 +5,10 @@ module.exports = function (app) {
   let { client, connection } = app.get('postgres');
   if(process.env.NODE_ENV === 'production') {
     connection = {
-      host : 'process.env.DB_HOST',
-      user : 'process.env.DB_USER',
-      password : 'process.env.DB_PWD',
-      database : 'process.env.DB_NAME',
+      host : process.env.DB_HOST,
+      user : process.env.DB_USER,
+      password : process.env.DB_PWD,
+      database : process.env.DB_NAME,
       ssl: true,
       rejectUnauthorized: false
     }
